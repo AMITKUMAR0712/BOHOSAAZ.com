03b1537c122509711de415f251f39647512bc3d2
AMITKUMAR0712 <amittalan2203@gmail.com>
2026-05-02 02:46:49 +0530

    looking so good let see

--- src/app/[lang]/admin/_components/AdminSidebar.tsx
+++ src/app/[lang]/admin/_components/AdminSidebar.tsx
@@ -3,6 +3,7 @@
 import * as React from "react";
 import Link from "next/link";
 import { usePathname } from "next/navigation";
+import { cn } from "@/lib/cn";
 import {
   BadgePercent,
   BookOpenText,
